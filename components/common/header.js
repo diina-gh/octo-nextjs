ea292fb84353cbf208015c4912a360faba271e5b
Seydina <dina3903@gmail.com>
2022-02-21 04:48:51 +0100

    update

--- components/common/header.js
+++ components/common/header.js
@@ -1,8 +1,43 @@
-import React, { Component, useState } from 'react';
+import React, { Component,Fragment, useState } from 'react';
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { UserIcon, ShoppingCartIcon, SearchIcon } from '@heroicons/react/outline'
+import { Dialog, Transition } from '@headlessui/react'
+import { UserIcon, ShoppingCartIcon, SearchIcon, XIcon } from '@heroicons/react/outline'
 
+const products = [
+    {
+      id: 1,
+      name: 'Throwback Hip Bag',
+      href: '#',
+      color: 'Salmon',
+      price: '$90.00',
+      quantity: 1,
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
+      imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
+    },
+    {
+      id: 2,
+      name: 'Medium Stuff Satchel',
+      href: '#',
+      color: 'Blue',
+      price: '$32.00',
+      quantity: 1,
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+      imageAlt:
+        'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
+    },
+    {
+        id: 3,
+        name: 'Throwback Hip Bag',
+        href: '#',
+        color: 'Salmon',
+        price: '$90.00',
+        quantity: 1,
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-03.jpg',
+        imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
+    },
+    // More products...
+  ]
 
 const style0 =' text-2xl xl:text-3xl font-bold gt-text-gradient self-center';
 const style1 = 'font-medium text-lg mx-3 hidden md:block gt-text-gradient duration-700 ease-in-out cursor-pointer gt-border1';
@@ -11,6 +46,7 @@ const style2 = 'font-medium text-lg mx-3 hidden md:block text-gray-900 gt-hover-
 export default function Header(props) {
 
     const router = useRouter();
+    const [open, setOpen] = useState(false)
 
     return (
         <header className="w-full h-20 bg-gray-100 bg-opacity-0 flex flex-col justify-center px-7 sm:px-16 md:px-20 lg:px-24 xl:px-28 2xl:px-48">
@@ -53,27 +89,137 @@ export default function Header(props) {
                 <div className='flex flex-row self-center'>
 
                     <div className="w-7 md:w-8 h-7 md:h-8 rounded-full  flex flex-row justify-center cursor-pointer gt-btn-type1 mr-2">
-                        <Link  href={'/'} >
-                            <SearchIcon className="w-3 md:w-4 h-3 md:h-4 text-white self-center"/>
-                        </Link>
+                        <SearchIcon className="w-3 md:w-4 h-3 md:h-4 text-white self-center"/>
+                    </div>
+
+                    <div onClick={() => setOpen(true)} className="w-7 md:w-8 h-7 md:h-8 rounded-full  flex flex-row justify-center cursor-pointer gt-btn-type1 mr-2">
+                        <ShoppingCartIcon className="w-3 md:w-4 h-3 md:h-4 text-white self-center"/>
                     </div>
 
                     <div className="w-7 md:w-8 h-7 md:h-8 rounded-full  flex flex-row justify-center cursor-pointer gt-btn-type1 mr-2">
-                        <Link  href={'/'} >
-                            <ShoppingCartIcon className="w-3 md:w-4 h-3 md:h-4 text-white self-center"/>
-                        </Link>
-                    </div>
-
-                    <div className="w-7 md:w-8 h-7 md:h-8 rounded-full  flex flex-row justify-center cursor-pointer gt-btn-type1 mr-2">
-                        <Link  href={'/'} >
-                            <UserIcon className="w-3 md:w-4 h-3 md:h-4 text-white self-center"/>
-                        </Link>
+                         <UserIcon className="w-3 md:w-4 h-3 md:h-4 text-white self-center"/>
                     </div>
 
                 </div>
 
-
             </div>
+
+        <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="fixed inset-0 overflow-hidden" onClose={setOpen}>
+            <div className="absolute inset-0 overflow-hidden">
+            <Transition.Child
+                as={Fragment}
+                enter="ease-in-out duration-500"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in-out duration-500"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+            >
+                <Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            <div className="fixed inset-y-0 right-0 pl-10 max-w-full flex">
+                <Transition.Child
+                as={Fragment}
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enterFrom="translate-x-full"
+                enterTo="translate-x-0"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
+                >
+                <div className="w-screen max-w-md">
+                    <div className="h-full flex flex-col bg-white shadow-xl overflow-y-scroll">
+                    <div className="flex-1 py-6 overflow-y-auto px-4 sm:px-6">
+                        <div className="flex items-start justify-between">
+                        <Dialog.Title className="text-lg font-medium text-gray-900">Shopping cart</Dialog.Title>
+                        <div className="ml-3 h-7 flex items-center">
+                            <button
+                            type="button"
+                            className="-m-2 p-2 text-gray-400 hover:text-gray-500"
+                            onClick={() => setOpen(false)}
+                            >
+                            <span className="sr-only">Close panel</span>
+                            <XIcon className="h-6 w-6" aria-hidden="true" />
+                            </button>
+                        </div>
+                        </div>
+
+                        <div className="mt-8">
+                        <div className="flow-root">
+                            <ul role="list" className="-my-6 divide-y divide-gray-200">
+                            {products.map((product) => (
+                                <li key={product.id} className="py-6 flex">
+                                <div className="flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden">
+                                    <img
+                                    src={product.imageSrc}
+                                    alt={product.imageAlt}
+                                    className="w-full h-full object-center object-cover"
+                                    />
+                                </div>
+
+                                <div className="ml-4 flex-1 flex flex-col">
+                                    <div>
+                                    <div className="flex justify-between text-base font-medium text-gray-900">
+                                        <h3>
+                                        <a href={product.href}>{product.name}</a>
+                                        </h3>
+                                        <p className="ml-4">{product.price}</p>
+                                    </div>
+                                    <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                                    </div>
+                                    <div className="flex-1 flex items-end justify-between text-sm">
+                                    <p className="text-gray-500">Qty {product.quantity}</p>
+
+                                    <div className="flex">
+                                        <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                        Remove
+                                        </button>
+                                    </div>
+                                    </div>
+                                </div>
+                                </li>
+                            ))}
+                            </ul>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
+                        <div className="flex justify-between text-base font-medium text-gray-900">
+                        <p>Subtotal</p>
+                        <p>$262.00</p>
+                        </div>
+                        <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
+                        <div className="mt-6">
+                        <a
+                            href="#"
+                            className="flex justify-center items-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                        >
+                            Checkout
+                        </a>
+                        </div>
+                        <div className="mt-6 flex justify-center text-sm text-center text-gray-500">
+                        <p>
+                            or{' '}
+                            <button
+                            type="button"
+                            className="text-indigo-600 font-medium hover:text-indigo-500"
+                            onClick={() => setOpen(false)}
+                            >
+                            Continue Shopping<span aria-hidden="true"> &rarr;</span>
+                            </button>
+                        </p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </Transition.Child>
+            </div>
+            </div>
+        </Dialog>
+        </Transition.Root>
                 
         </header>
     );
