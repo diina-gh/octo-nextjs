500d306dca1249db4553d9b09be122d81673962d
Seydina <dina3903@gmail.com>
2022-02-22 01:56:40 +0100

    update

--- components/common/header.js
+++ components/common/header.js
@@ -7,33 +7,33 @@ import { UserIcon, ShoppingCartIcon, SearchIcon, HeartIcon, XIcon } from '@heroi
 const products = [
     {
       id: 1,
-      name: 'Throwback Hip Bag',
+      name: 'Macbook Pro',
       href: '#',
-      color: 'Salmon',
-      price: '$90.00',
+      color: 'Gris',
+      price: '$780 000 CFA',
       quantity: 1,
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
+      imageSrc: 'images/product/product1.png',
       imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
     },
     {
       id: 2,
-      name: 'Medium Stuff Satchel',
+      name: 'Ipad Air',
       href: '#',
-      color: 'Blue',
-      price: '$32.00',
+      color: 'Noir',
+      price: '880 000 CFA',
       quantity: 1,
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+      imageSrc: 'images/product/product4.png',
       imageAlt:
         'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
     },
     {
         id: 3,
-        name: 'Throwback Hip Bag',
+        name: 'Iphone 13 Pro',
         href: '#',
-        color: 'Salmon',
-        price: '$90.00',
+        color: 'Gris',
+        price: '575 000 CFA',
         quantity: 1,
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-03.jpg',
+        imageSrc: 'images/product/product3.png',
         imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
     },
     // More products...
@@ -205,14 +205,14 @@ export default function Header(props) {
                     leaveTo="translate-x-full"
                     >
                     <div className="w-screen max-w-md">
-                        <div className="h-full flex flex-col bg-white shadow-xl overflow-y-scroll">
+                        <div className="h-full flex flex-col bg-gray-900 shadow-xl overflow-y-scroll">
                         <div className="flex-1 py-6 overflow-y-auto px-4 sm:px-6">
                             <div className="flex items-start justify-between">
-                            <Dialog.Title className="text-lg font-medium text-gray-900">Shopping cart</Dialog.Title>
+                            <Dialog.Title className="text-lg font-medium text-gray-100">Mon panier (3)</Dialog.Title>
                             <div className="ml-3 h-7 flex items-center">
                                 <button
                                 type="button"
-                                className="-m-2 p-2 text-gray-400 hover:text-gray-500"
+                                className="-m-2 p-2 text-gray-400 hover:text-gray-300"
                                 onClick={() => setOpen(false)}
                                 >
                                 <span className="sr-only">Close panel</span>
@@ -223,32 +223,30 @@ export default function Header(props) {
 
                             <div className="mt-8">
                             <div className="flow-root">
-                                <ul role="list" className="-my-6 divide-y divide-gray-200">
+                                <ul role="list" className="-my-6 divide-y divide-gray-600">
                                 {products.map((product) => (
                                     <li key={product.id} className="py-6 flex">
-                                    <div className="flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden">
-                                        <img
-                                        src={product.imageSrc}
-                                        alt={product.imageAlt}
-                                        className="w-full h-full object-center object-cover"
-                                        />
+                                    <div className="flex-shrink-0 w-24 h-24 flex flex-row justify-center rounded-xl bg-gray-400 bg-opacity-20 overflow-hidden">
+                                        <div className='p-image4 self-center'>
+                                            <img src={product.imageSrc} alt={product.imageAlt} />
+                                        </div>
                                     </div>
 
                                     <div className="ml-4 flex-1 flex flex-col">
                                         <div>
-                                        <div className="flex justify-between text-base font-medium text-gray-900">
+                                        <div className="flex justify-between text-base font-medium text-gray-100">
                                             <h3>
                                             <a href={product.href}>{product.name}</a>
                                             </h3>
                                             <p className="ml-4">{product.price}</p>
                                         </div>
-                                        <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                                        <p className="mt-1 text-sm text-gray-400">{product.color}</p>
                                         </div>
                                         <div className="flex-1 flex items-end justify-between text-sm">
-                                        <p className="text-gray-500">Qty {product.quantity}</p>
+                                        <p className="text-gray-300">Qty {product.quantity}</p>
 
                                         <div className="flex">
-                                            <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                            <button type="button" className="font-medium text-red-600 hover:text-red-500">
                                             Remove
                                             </button>
                                         </div>
@@ -261,7 +259,7 @@ export default function Header(props) {
                             </div>
                         </div>
 
-                        <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
+                        <div className="border-t border-gray-600 py-6 px-4 sm:px-6">
                             <div className="flex justify-between text-base font-medium text-gray-900">
                             <p>Subtotal</p>
                             <p>$262.00</p>
@@ -270,20 +268,20 @@ export default function Header(props) {
                             <div className="mt-6">
                             <a
                                 href="#"
-                                className="flex justify-center items-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                                className="flex justify-center items-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white gt-bg-gradient"
                             >
-                                Checkout
+                                Commander
                             </a>
                             </div>
                             <div className="mt-6 flex justify-center text-sm text-center text-gray-500">
                             <p>
-                                or{' '}
+                                ou{' '}
                                 <button
                                 type="button"
-                                className="text-indigo-600 font-medium hover:text-indigo-500"
+                                className="text-indigo-300 font-medium hover:text-indigo-500"
                                 onClick={() => setOpen(false)}
                                 >
-                                Continue Shopping<span aria-hidden="true"> &rarr;</span>
+                                Poursuivre vos achats<span aria-hidden="true"> &rarr;</span>
                                 </button>
                             </p>
                             </div>
