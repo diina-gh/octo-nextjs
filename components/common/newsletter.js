da991f425a5270afbd5c803a8e43e26994c23ab0
Seydina <dina3903@gmail.com>
2022-02-23 03:36:38 +0100

    update

--- components/common/newsletter.js
+++ components/common/newsletter.js
@@ -13,7 +13,7 @@ export default function Newsletter(props) {
                         <div className="w-full flex flex-row justify-center my-5">
                             <div className="w-10/12 md:w-1/2 h-12 md:h-14 bg-white opacity-100 shadow-xl rounded-md px-2 py-2 flex flex-row justify-between border-1 border-gray-200 border-opacity-40">
                                 <div className="h-full w-8/12 md:w-3/4 2xl:w-5/6">
-                                    <input className="h-full w-full px-2 md:px-4 text-sm md:text-base border-0 ring-0" type="email" id="" name="" placeholder="Ecrire votre adresse email.." required />
+                                    <input className="border-0 focus:ring-0 h-full w-full px-2 md:px-4 text-sm md:text-base" type="email" id="" name="" placeholder="Ecrire votre adresse email.." required />
                                 </div>
                                 <div className="h-full w-4/12 md:w-1/4 2xl:w-1/6 gt-btn-type1 cursor-pointer rounded-md opacity-100 flex flex-col justify-center">
                                     <button type="submit" className="text-center text-white text-sm md:text-base">S&#039;inscrire</button>
