3a174ede00098322b1ff3fb8cb172d8de279b3ba
Seydina <dina3903@gmail.com>
2022-02-24 03:59:21 +0100

    update

--- components/ui/Others/sectionHeader2.js
+++ components/ui/Others/sectionHeader2.js
@@ -11,8 +11,8 @@ export default function SectionHeader2(props) {
                 <svg viewBox="0 0 52 24" fill="currentColor" className="absolute -top-2 left-0 z-0 hidden w-32 -mt-8 -ml-20 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
                     <defs>
                         <linearGradient id="Gradient2" x1="0" x2="0" y1="0" y2="1">
-                            <stop offset="45%" stop-color="#2564eb1e"/>
-                            <stop offset="55%" stop-color="#6d27d769"/>
+                            <stop offset="45%" stopColor="#2564eb1e"/>
+                            <stop offset="55%" stopColor="#6d27d769"/>
                         </linearGradient>
                         <pattern id="70326c9b-4a0f-429b-9c76-792941e326d5" x="0" y="0" width=".135" height=".30">
                             <circle cx="2" cy="1" r=".75" fill="url(#Gradient2)" />
