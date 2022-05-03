17359b4c55616cddf7381649fc71f66b0ae2efe9
Seydina <dina3903@gmail.com>
2022-05-04 01:53:08 +0200

    update

--- components/ui/Buttons/buttonType4.js
+++ components/ui/Buttons/buttonType4.js
@@ -3,7 +3,7 @@ import React, { useRef, useState } from "react";
 export default function ButtonType4(props) {
 
   return (
-    <div style={{marginRight: props.mr ? props.mr : 0 }} className='px-6 h-12 md:h-14 flex flex-row justify-center text-base md:text-lg font-medium text-gtpurple hover:text-gtblue duration-700 ease-in-out cursor-pointer'>
+    <div style={{marginRight: props.mr ? props.mr : 0 }} className='px-6 h-12 md:h-14 flex flex-row justify-center text-base md:text-lg font-medium text-gtpurple hover:text-gtblue transition duration-700 ease-in-out cursor-pointer'>
         <div className='self-center underline'>{props.text}</div>
     </div>
   );
